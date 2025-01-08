a2561a45f7fdacbacfac8ddd0f5fa5125b33b1dc
Roberto <oliveirarobertw@gmail.com>
2025-01-08 20:10:01 -0300

    ex 22 e 23 resolvidos

--- Workspace - Python/curso-em-video/aula009-manipulando-texto/ex022.py
+++ Workspace - Python/curso-em-video/aula009-manipulando-texto/ex022.py
@@ -1,5 +1,18 @@
 # Um programa que leia o nome completo de uma pessoa e mostre
 # O nome com todas as letras maiuculas
 # O nome com todas as letras minusculas
-# Quatas letras ao todo (Desconsiderar espaço)
+# Quantas letras ao todo (Desconsiderar espaço)
 # Quantas letras tem o primeiro nome
+
+
+nome = input('Qual o seu nome completo?')
+
+maiusculo = nome.upper()
+minusculo = nome.lower()
+primeironome = nome.split()[0]
+letrasprimeironome = len(primeironome)
+
+print(maiusculo)
+print(minusculo)
+print(primeironome)
+print(letrasprimeironome)
--- Workspace - Python/curso-em-video/aula009-manipulando-texto/ex023.py
+++ Workspace - Python/curso-em-video/aula009-manipulando-texto/ex023.py
@@ -1,1 +1,11 @@
 # Ler os números de 0 a 9999 e mostra na tela cada um dos dígitos separados (Unidade, dezena, centena, milhar)
+
+numero = int(input('Digite um número entre 0 e 9999: \n'))
+
+unidade = numero % 10
+dezena = (numero // 10) % 10
+centena = (numero // 100) % 10
+milhar = (numero // 1000) % 10
+
+print(f"Numero: {numero} | Milhar: {milhar} | Centena: {
+      centena} | Dezena: {dezena} | Unidade: {unidade}")
