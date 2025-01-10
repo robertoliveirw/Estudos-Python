cad48dfe724e5e2fd924c0b067a9e73c66261b97
Roberto <oliveirarobertw@gmail.com>
2025-01-09 22:10:53 -0300

    ex 29 Finalizado

--- Workspace - Python/curso-em-video/Exercícios/ex29.py
+++ Workspace - Python/curso-em-video/Exercícios/ex29.py
@@ -1,1 +1,15 @@
 # ler a velocidade de um carro e aplicar uma multa caso esteja acima de 80km/h de 7 reais por km acima
+
+import random
+
+velocidade = random.randint(0, 120)
+
+print(f'Você passou a: {velocidade}Km/h')
+
+if velocidade <= 80:
+    print('Dentro do limite de velocidade')
+else:
+    kmamais = velocidade - 80
+    multa = kmamais * 7
+    print(f'Você passou a {velocidade}Km/h, sendo isso {
+          kmamais}Km/h acima do limite permitido. Sua multa é de {multa}')
