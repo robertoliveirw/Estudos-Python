fe5c584a456ca38d177987fd4c7ab42853cd1595
Roberto <oliveirarobertw@gmail.com>
2025-01-09 22:05:06 -0300

    Finalizado ex 28

--- Workspace - Python/curso-em-video/Exercícios/ex28.py
+++ Workspace - Python/curso-em-video/Exercícios/ex28.py
@@ -1,1 +1,10 @@
-#Pensar (entre 0 e 5) em um número e dizer se o usuário acertou ou não
+# Pensar (entre 0 e 5) em um número e dizer se o usuário acertou ou não
+
+import random
+numeroaleatorio = random.randint(0, 5)
+numero = int(input('Em qual número de 0 a 5 eu estou pensando? '))
+
+if numeroaleatorio == numero:
+    print('Você acertou!')
+else:
+    print(f'Você errou, estou pensando no número {numeroaleatorio}')
