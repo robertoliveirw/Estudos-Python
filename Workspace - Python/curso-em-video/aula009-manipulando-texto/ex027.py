f34d405704707e5d482a789cb726d473489132e8
Roberto <oliveirarobertw@gmail.com>
2025-01-09 21:23:31 -0300

    ex27

--- Workspace - Python/curso-em-video/aula009-manipulando-texto/ex027.py
+++ Workspace - Python/curso-em-video/aula009-manipulando-texto/ex027.py
@@ -1,1 +1,10 @@
 # ler o nome completo de uma pessoa, mostrar o primeiro e último nome
+# O strip é para eliminar espaços antes e dps
+nome = str(input('Qual o seu nome completo? \n')).strip()
+
+listanome = nome.split()
+primeironome = listanome[0]
+ultimonome = listanome[-1]
+
+print(f"Primeiro nome: {primeironome}")
+print(f"Último nome: {ultimonome}")
