de30934db46966a7381af326bb0aad6f1b87519f
Roberto <oliveirarobertw@gmail.com>
2025-01-09 22:22:10 -0300

    ex34 Finalizado

--- Workspace - Python/curso-em-video/Exercícios/ex34.py
+++ Workspace - Python/curso-em-video/Exercícios/ex34.py
@@ -1,1 +1,10 @@
-# ler o o salário de um funcionário. Se for acima de 1250 acresser 10% se for abaixo, acresser 15%
+# ler o o salário de um funcionário. Se for acima de 1250 acrescer 10% se for abaixo, acrescer 15%
+
+salario = float(input('Qual o seu salário? \n'))
+
+if salario > 1250:
+    novosal = salario*1.1
+    print(f'Seu novo salário é de R$ {novosal:.2f}')
+else:
+    novosal = salario*1.15
+    print(f'Seu novo salário é de R$ {novosal:.2f}')
