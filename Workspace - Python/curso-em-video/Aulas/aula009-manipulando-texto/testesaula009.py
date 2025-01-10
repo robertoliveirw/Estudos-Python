f32567607f8398c3cfb32b5da713787d79ab865f
Roberto <oliveirarobertw@gmail.com>
2025-01-09 21:32:20 -0300

    Finalizado

--- Workspace - Python/curso-em-video/Aulas/aula009-manipulando-texto/testesaula009.py
+++ Workspace - Python/curso-em-video/Aulas/aula009-manipulando-texto/testesaula009.py
@@ -3,3 +3,4 @@
 nome = 'Roberto de Oliveira'
 
 print(nome[0:5:2])
+# O primeiro número indica que vai começar da posição 0 - início (R), o segundo é o final até a posição 5 (r) e por fim, o 2 significa que vai pular de dois em dois
