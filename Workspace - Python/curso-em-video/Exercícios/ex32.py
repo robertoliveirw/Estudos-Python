ae2857fed3cbdb6b8641a4617f753597d0ee9542
Roberto <oliveirarobertw@gmail.com>
2025-01-11 10:41:40 -0300

    ex32 finalizado

--- Workspace - Python/curso-em-video/Exercícios/ex32.py
+++ Workspace - Python/curso-em-video/Exercícios/ex32.py
@@ -1,1 +1,8 @@
 # ler se o ano é bissexto
+ano = int(input("Digite um ano: "))
+
+
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print(f"O ano {ano} é bissexto.")
+else:
+    print(f"O ano {ano} NÃO é bissexto.")
