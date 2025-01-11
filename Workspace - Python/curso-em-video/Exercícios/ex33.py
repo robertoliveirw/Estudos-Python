2decebe44eda82988722a3316cbb973439d9582d
Roberto <oliveirarobertw@gmail.com>
2025-01-11 10:40:55 -0300

    ex 33 finalizado

--- Workspace - Python/curso-em-video/Exercícios/ex33.py
+++ Workspace - Python/curso-em-video/Exercícios/ex33.py
@@ -1,1 +1,13 @@
 # ler o3 números e ver qual é o menor e qual o maior?
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+n3 = float(input("Digite o terceiro número: "))
+
+
+menor = min(n1, n2, n3)
+maior = max(n1, n2, n3)
+
+
+print(f"O menor número é: {menor}")
+print(f"O maior número é: {maior}")
--- Workspace - Python/curso-em-video/Exercícios/ex35.py
+++ Workspace - Python/curso-em-video/Exercícios/ex35.py
@@ -4,7 +4,6 @@ r1 = float(input("Reta 1: "))
 r2 = float(input("Reta 2: "))
 r3 = float(input("Reta 3: "))
 
-# Verifica se forma um triângulo
 if r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1:
     print("As três retas podem formar um triângulo.")
 else:
