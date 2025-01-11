e17ffa2185082057311867360db01c8daff43481
Roberto <oliveirarobertw@gmail.com>
2025-01-11 19:05:58 -0300

    Capítulo-02-Classes-e-Objetos-Funçao-ID

---
+++ Workspace - Python/fatec/capitulo02-classes-e-objetos.py
@@ -0,0 +1,7 @@
+# Neste capítulo está sendo falado sobre classes e objetos, e em dado momento foi falado a respeito da função ID.{retorna um valor numérico único que identifica um objeto armazenado na memória}
+
+a = 10
+
+id(a)
+
+# se o valor for o mesmo, para otimizar a memoria, o id também é o mesmo.
