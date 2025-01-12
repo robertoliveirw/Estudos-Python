8e483a7a95f6969ae36ba0a33b6bce8d3efec30e
Roberto <oliveirarobertw@gmail.com>
2025-01-12 20:34:00 -0300

    teste

--- Workspace - Python/fatec/Capitulos/testes.py
+++ Workspace - Python/fatec/Capitulos/testes.py
@@ -1,17 +1,4 @@
-# lp = 'Python'
-# c = lp[3]
-# if c <= 'e':
-#    print('a-e')
-# elif c <= 'j':
-#    print('f-j')
-# else:
-#    print('k-z')
-
-
-a = 3
-b = 2
-if a ** b > b ** a:
-    if a % b == a // 1:
-        print('UM')
-else:
-    print('DOIS')
+x = 3
+while x < 16:
+    x = x * 4
+print(x)
