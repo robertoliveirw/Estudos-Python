0f0ca5c9a4d1024452e7fbdf9c35f454a30c94b6
Roberto <oliveirarobertw@gmail.com>
2025-01-12 20:08:09 -0300

    continue

---
+++ Workspace - Python/fatec/Capitulos/05-repeticoes/05-repeticoes-continue.py
@@ -0,0 +1,9 @@
+# Posso adicionar o 'continue' para pular algo do programa:
+
+i = 0
+
+while i < 5:
+    i = i + 1
+    if i == 4:
+        continue
+    print(i, end=' ')
--- Workspace - Python/fatec/Capitulos/05-repeticoes/05-repeticoes.py
+++ Workspace - Python/fatec/Capitulos/05-repeticoes/05-repeticoes.py
@@ -1,17 +1,7 @@
-# print('Início do Programa')
+print('Início do Programa')
 
-# numero = 1
-# while numero <= 10:
-#    print(numero)
-#    numero = numero + 1
-# print('Fim do Programa')
-
-# Posso adicionar o 'continue' para pular algo do programa:
-
-i = 0
-
-while i < 5:
-    i = i + 1
-    if i == 4:
-        continue
-    print(i, end=' ')
+numero = 1
+while numero <= 10:
+    print(numero)
+    numero = numero + 1
+print('Fim do Programa')
