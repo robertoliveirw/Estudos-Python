53c47a6fff24f0224960cf49cf45de072014fdaa
Roberto <oliveirarobertw@gmail.com>
2025-01-12 14:38:55 -0300

    condicional

---
+++ Workspace - Python/fatec/Capitulos/04-condicional.py
@@ -0,0 +1,8 @@
+a = int(input('Digite um número: '))
+b = int(input('Digite um número: '))
+
+if b == 0:
+    print('A divisão não pode ser realizada por 0. \n Digite outro número!')
+else:
+    r = a / b
+    print(f' O resultado da divisão é: {r}')
