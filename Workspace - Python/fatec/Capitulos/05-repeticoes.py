469849ac9036bda210fffa1b577a854b8e69d8df
Roberto <oliveirarobertw@gmail.com>
2025-01-12 20:06:54 -0300

    reorganizando

--- Workspace - Python/fatec/Capitulos/05-repeticoes.py
+++
@@ -1,7 +0,0 @@
-print('Início do Programa')
-
-numero = 1
-while numero <= 10:
-    print(numero)
-    numero = numero + 1
-print('Fim do Programa')
---
+++ Workspace - Python/fatec/Capitulos/05-repeticoes/05-repeticoes.py
@@ -0,0 +1,17 @@
+# print('Início do Programa')
+
+# numero = 1
+# while numero <= 10:
+#    print(numero)
+#    numero = numero + 1
+# print('Fim do Programa')
+
+# Posso adicionar o 'continue' para pular algo do programa:
+
+i = 0
+
+while i < 5:
+    i = i + 1
+    if i == 4:
+        continue
+    print(i, end=' ')
