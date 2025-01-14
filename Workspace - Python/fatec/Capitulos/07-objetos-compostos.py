ff50db2aae4601e24621c03d490e9c165214b731
Roberto <oliveirarobertw@gmail.com>
2025-01-13 21:23:44 -0300

    exercicios finalizado

--- Workspace - Python/fatec/Capitulos/07-objetos-compostos.py
+++ Workspace - Python/fatec/Capitulos/07-objetos-compostos.py
@@ -1,3 +1,20 @@
 # Ler vários números e adicionar a uma lista
 
+
 numeros = []
+
+
+while True:
+
+    entrada = input(
+        'Digite um número para adicionar a lista ou digite \'FIM\' para finalizar.').strip()
+
+    if entrada.upper() == 'FIM':
+        break
+    try:
+        numero = float(entrada)
+        numeros.append(numero)
+    except ValueError:
+        print('Comando Inválido. Digite \'FIM\' para finalizar ou ou digite um número.')
+
+print('A lista de números é:', numeros)
