8b0378e09d753fb749ec2387bec511e8474a525b
Roberto <oliveirarobertw@gmail.com>
2025-01-13 21:32:43 -0300

    teste

--- Workspace - Python/fatec/Capitulos/testes.py
+++ Workspace - Python/fatec/Capitulos/testes.py
@@ -1,4 +1,14 @@
-x = 3
-while x < 16:
-    x = x * 4
-print(x)
+
+n = 3
+p = 'CPS'
+
+# Comando 1: p = n + p
+# Isso dará erro
+
+
+# Comando 2: n = p - n
+# Isso também dará erro
+
+# Comando 3: p *= n
+p *= n
+print(p)
