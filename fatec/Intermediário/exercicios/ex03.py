1116df3222a01d9727d9673177e13ed3bf8d5a6e
Roberto <oliveirarobertw@gmail.com>
2025-01-15 17:40:51 -0300

    ex03-bug

--- fatec/Intermediário/exercicios/ex03.py
+++ fatec/Intermediário/exercicios/ex03.py
@@ -6,7 +6,10 @@ from random import randint
 qtde = int(input('Qual a quantidade de elementos a ser criada entre de 1 até 50?'))
 conjunto = set()
 
-while len(conjunto) < qtde:
-    conjunto.add(randint(1,50))
+if len(conjunto) <= 50:
+    while len(conjunto) < qtde:
+        conjunto.add(randint(1,50))
 
-print(f'O Conjunto tem 1 elemento. Sendo ele: {conjunto}') if len(conjunto) == 1 else print(f'O Conjunto tem {len(conjunto)} elementos. Sendo eles: {conjunto} ')
+    print(f'O Conjunto tem 1 elemento. Sendo ele: {conjunto}') if len(conjunto) == 1 else print(f'O Conjunto tem {len(conjunto)} elementos. Sendo eles: {conjunto} ')
+else: 
+    print('Número Inválido. Digite algo no intervalo de 1 até 50')
