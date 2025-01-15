edfdbe8df309d523b04452b0d00bd19122a5be35
Roberto <oliveirarobertw@gmail.com>
2025-01-15 11:13:34 -0300

    bugarrumado

--- Workspace - Python/fatec/Intermediário/exercicios/ex01.py
+++ Workspace - Python/fatec/Intermediário/exercicios/ex01.py
@@ -9,33 +9,35 @@ while produto != 0:
     produto = int(input('Qual o código do produto que você quer verificar? \n digite \'0\' para finalizar.'))
 
     match produto:
-        Case 16: 
+        case 16: 
             print('Bebê')
-        Case 23: 
+        case 23: 
             print('Infantil Feminino')
-        Case 25: 
+        case 25: 
             print('Infantil masculino')
-        Case 29: 
+        case 29: 
             print('Infantil esportivo')
-        Case 42: 
+        case 42: 
             print('Masculino Formal')
-        Case 43: 
+        case 43: 
             print('Masculino Casual')
-        Case 49: 
+        case 49: 
             print('Masculino Esportivo')
-        Case 52: 
+        case 52: 
             print('Femino Formal Salto Baixo')
-        Case 53: 
+        case 53: 
             print('Femino Formal Salto Alto')
-        Case 55: 
+        case 55: 
             print('Feminino casual salto baixo')
-        Case 56: 
+        case 56: 
             print('Feminino Casual salto alto')
-        Case 59: 
+        case 59: 
             print('Feminino esportivo')
+        case 0:
+            print(' \n Programa Finalizado')
         case _ :
             print('Inválido')
     
-print(' \n Programa Finalizado')
+
 
     
