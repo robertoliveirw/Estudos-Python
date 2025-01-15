0bdc8836cd59dbfed09feb0c65ba162619ec5193
Roberto <oliveirarobertw@gmail.com>
2025-01-15 13:32:21 -0300

    ajustenome

--- fatec/Intermediário/capítulos/capitulo-09/09-conjuntos-classe-set.py
+++ fatec/Intermediário/capítulos/capitulo-09/09-conjuntos-classe-set.py
@@ -8,3 +8,7 @@ print(C1, C2, C3)
 print(len(C1), len(C2), len(C3))
 
 C4 = {} # Isso não é um conjunto, é um dicionário;
+
+nome = 'Roberto Eugenio Lopes Prazeres de Oliveira'
+C5 = set(nome)
+print(C5)
