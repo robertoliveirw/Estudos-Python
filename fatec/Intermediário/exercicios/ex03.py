e4bfd513d34140dcf6303ab09e0e71bbcfa5afc9
Roberto <oliveirarobertw@gmail.com>
2025-01-15 17:26:55 -0300

    Create ex03.py

---
+++ fatec/Intermediário/exercicios/ex03.py
@@ -0,0 +1,10 @@
+# Escreva um programa que leia um inteiro Qtde e crie um conjunto com elementos numéricos inteiros aleatórios dentro do intervalo fechado [1, 50]. Mostre o conjunto gerado na tela. Lembre-se que os conjuntos não podem ter elementos repetidos, então a geração de números aleatórios pode representar um problema. Como resolver isso?
+# Cuidado: Este programa tem potencial para entrar em laço infinito caso o valor fornecido para Qtde seja maior que 50. 
+
+from random import randint
+
+qtde = int(input('Qual a quantidade de elementos a ser criada entre de 1 até 50?'))
+conjunto = set()
+
+
+
