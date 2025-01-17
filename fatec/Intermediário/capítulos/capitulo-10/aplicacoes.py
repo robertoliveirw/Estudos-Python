81ab75ea47d06f6c867f87cfad55feffae0d98da
Roberto <oliveirarobertw@gmail.com>
2025-01-17 19:43:42 -0300

    finalizado

--- fatec/Intermediário/capítulos/capitulo-10/aplicacoes.py
+++ fatec/Intermediário/capítulos/capitulo-10/aplicacoes.py
@@ -3,3 +3,17 @@
 UF = {}
 
 print('Leitura de dados \n')
+while True:
+    sigla = input('Digite a Sigla: ')
+    if sigla == '':
+        break
+    elif sigla in UF:
+        print(f'A sigla {sigla} já existe no cadastro.')
+        continue
+    estado = input('Digite o estado: ')
+    capital = input('Digite a capital: ')
+    pib = float(input('Digite o PIB: '))
+
+    UF[sigla] = ((estado, capital, pib))
+
+print(UF)
