378c7bb3d1174aa20d212b2845809586ded23fda
Roberto <oliveirarobertw@gmail.com>
2025-01-17 17:39:03 -0300

    Update iteracoes.py

---
+++ fatec/Intermediário/capítulos/capitulo-10/iteracoes.py
@@ -0,0 +1,13 @@
+# Iteção - Falar os itens do dicionário e a sua palavra-chave
+
+UF = {
+    'MA' : 'Maranhão',
+    'PA':'Pará',
+}
+
+print('Início do Programa \n O dicionário é esse:')
+
+for X in UF:
+    print(f'{X} - {UF[X]}')
+
+print('\n Fim do programa')
