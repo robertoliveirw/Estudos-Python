94f8f56a91d64976ac9ab846875667e37209e349
Roberto <oliveirarobertw@gmail.com>
2025-01-17 11:18:37 -0300

    reestruturandp

--- fatec/Intermediário/capítulos/capitulo-10/adicionando-itens-no-dicionario.py
+++ fatec/Intermediário/capítulos/capitulo-10/adicionando-itens-no-dicionario.py
@@ -2,38 +2,43 @@
 
 UF = {}
 
-#Norte 
+#Norte
 UF['AC'] = 'Acre'
 UF['AM'] = 'Amazonas'
 UF['AP'] = 'Amapá'
-UF['RO'] = 'Rondonia'
-UF['RR'] = 'Roraima'
 UF['PA'] = 'Pará'
+UF['RO'] = 'Rondônia'
+UF['RR'] = 'Roraima'
 UF['TO'] = 'Tocantins'
+
 #Nordeste
 UF['AL'] = 'Alagoas'
 UF['BA'] = 'Bahia'
 UF['CE'] = 'Ceará'
 UF['MA'] = 'Maranhão'
-UF['SE'] = 'Sergipe'
-UF['RN'] = 'Rio Grande do Norte'
-UF['PI'] = 'Piauí'
-UF['PE'] = 'Pernambuco'
 UF['PB'] = 'Paraíba'
+UF['PE'] = 'Pernambuco'
+UF['PI'] = 'Piauí'
+UF['RN'] = 'Rio Grande do Norte'
+UF['SE'] = 'Sergipe'
+
 #Centro-Oeste
-UF['GO'] = 'Goiás'
 UF['DF'] = 'Distrito Federal'
-UF['MT'] = 'Mato Grosso'
+UF['GO'] = 'Goiás'
 UF['MS'] = 'Mato Grosso do Sul'
+UF['MT'] = 'Mato Grosso'
+
 #Sudeste
 UF['ES'] = 'Espírito Santo'
-UF['RJ'] = 'Rio de Janeiro'
 UF['MG'] = 'Minas Gerais'
+UF['RJ'] = 'Rio de Janeiro'
 UF['SP'] = 'São Paulo'
+
 #Sul
-UF['SC'] = 'Santa Catarina'
-UF['RS'] = 'Rio Grande do Sul'
 UF['PR'] = 'Paraná'
+UF['RS'] = 'Rio Grande do Sul'
+UF['SC'] = 'Santa Catarina'
+
 
 
 
