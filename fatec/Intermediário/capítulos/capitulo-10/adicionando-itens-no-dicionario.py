6bee8eb3c5b79b16e9c18a0159cd6b4c83d0b978
Roberto <oliveirarobertw@gmail.com>
2025-01-17 11:18:07 -0300

    adicionando estados na lista

---
+++ fatec/Intermediário/capítulos/capitulo-10/adicionando-itens-no-dicionario.py
@@ -0,0 +1,43 @@
+# Adicionando itens no Dicionário
+
+UF = {}
+
+#Norte 
+UF['AC'] = 'Acre'
+UF['AM'] = 'Amazonas'
+UF['AP'] = 'Amapá'
+UF['RO'] = 'Rondonia'
+UF['RR'] = 'Roraima'
+UF['PA'] = 'Pará'
+UF['TO'] = 'Tocantins'
+#Nordeste
+UF['AL'] = 'Alagoas'
+UF['BA'] = 'Bahia'
+UF['CE'] = 'Ceará'
+UF['MA'] = 'Maranhão'
+UF['SE'] = 'Sergipe'
+UF['RN'] = 'Rio Grande do Norte'
+UF['PI'] = 'Piauí'
+UF['PE'] = 'Pernambuco'
+UF['PB'] = 'Paraíba'
+#Centro-Oeste
+UF['GO'] = 'Goiás'
+UF['DF'] = 'Distrito Federal'
+UF['MT'] = 'Mato Grosso'
+UF['MS'] = 'Mato Grosso do Sul'
+#Sudeste
+UF['ES'] = 'Espírito Santo'
+UF['RJ'] = 'Rio de Janeiro'
+UF['MG'] = 'Minas Gerais'
+UF['SP'] = 'São Paulo'
+#Sul
+UF['SC'] = 'Santa Catarina'
+UF['RS'] = 'Rio Grande do Sul'
+UF['PR'] = 'Paraná'
+
+
+
+
+
+
+print(UF)
