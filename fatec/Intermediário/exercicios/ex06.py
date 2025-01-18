02d8b52d386ddd3df8dade79f4eb031614e72ce8
Roberto <oliveirarobertw@gmail.com>
2025-01-18 12:05:10 -0300

    finalizado

--- fatec/Intermediário/exercicios/ex06.py
+++ fatec/Intermediário/exercicios/ex06.py
@@ -1,23 +1,20 @@
-''' Escreva um prograsma que leia do teclado o código de um produto e seu preço unitário. O código é uma string e o preço é real. Acrescente o par código: Preço em um dicionário. O programa deve verificar se o cpodigo já está no dicionário e nesete caso deve emitir uma mensagem de erro. O laço termina quando for fornecido um string vazio para o código., Ao final, exibir código e preço, em um produto em cada linha'''
+''' Escreva um programa que leia do teclado dados dos Estados Brasileiros. Siglam, nome, capital e pib. A sigla deve ser usada como chave para o dicionário e o valor deve ser uma tupla formada com nome capital e pib. Finalizar o código quando uma sitring vazia der entrada.'''
 
-item = {}
+UF = {}
 
 print('Início do Programa')
 
 while True:
-     
-    codigo = input('Digite o código do produto: ')
-
-    if codigo == '':
-        print('\n Fim do programa.')
+    sigla = input('Digite a sigla do estado: ')
+    if sigla == '':
+        print(f'\n Fim do programa.')
         break
-    elif codigo in item:
-        print(f'\n o código {codigo} já existe no programa.')
-         
+    elif sigla in UF:
+        print(f'\n A sigla:  {sigla} já existe no programa.')
+        continue 
+    estado = input('Digite o nome do estado: ')
+    capital = input('Digite a capital do estado: ')
+    pib = float(input('Digite o pib do estado: '))
+    UF[sigla] = ((estado, capital, pib))
 
-    preco = float(input('Digite o preço do produto: '))
-    item[codigo] = ((preco))
-
-print(f'\n Esta é a lista de Produtos(codigos) e Preços: {item}')
-    
-   
+print(f'A lista é essa: {UF}')   
