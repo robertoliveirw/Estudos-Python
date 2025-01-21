6259f0bc87637f7f0b28a08a9824da83d7fe093f
Roberto <oliveirarobertw@gmail.com>
2025-01-21 18:30:06 -0300

    pandas

---
+++ teste/testedepandas.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+dicionario_produtos = {
+    'nome': ['iphone', 'ipad', 'airpod'],
+    'preco': [5000, 9000, 2000], 
+    'estoque': [100, 50, 60] 
+}    
+lita_produtos = [
+    {'nome': 'iphone', 'preco':5000, 'estoque':100},
+    {'nome': 'ipad', 'preco':9000, 'estoque':50},
+    {'nome': 'airpod', 'preco':2000, 'estoque':60}
+]
