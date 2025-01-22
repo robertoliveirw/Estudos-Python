dd384be918bf692e1a103b3b131fa0636309f9a2
Roberto <oliveirarobertw@gmail.com>
2025-01-22 18:53:03 -0300

    Create ex07..py

---
+++ fatec/Intermediário/exercicios/ex07..py
@@ -0,0 +1,4 @@
+'''Enuciado:
+Escreva um programa que permaneça que leia um arquivo de entrada, sabendo que esse arquivo tem um número inteiro em cada linha. Todos os números lidos devem ser mostrados na tela. Mostrar também a soma dos valores, a quantidade, a média aritmética, o menor valor e o maior valor. Usar um laco while e na leitura usar o método .readline()'''
+
+lista = []
