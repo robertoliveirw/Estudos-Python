3249d3ad4d45b4883e24a3883e0a9b12fcd92832
Roberto <oliveirarobertw@gmail.com>
2025-01-22 19:20:10 -0300

    Create ex09..py

---
+++ fatec/Intermediário/exercicios/ex09..py
@@ -0,0 +1,4 @@
+'''Exercício Resolvido 11.3
+Escreva um programa que permaneça em laço lendo números reais até que seja digitado 0. Todos os valores digitados, exceto o zero, devem ser gravados em um arquivo em disco, um por linha, com 3 casas decimais. 
+Usar o método . write ()'''
+
