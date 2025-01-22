ef6a1d8578f27cccab0ccd0bc61f81c7e01d6087
Roberto <oliveirarobertw@gmail.com>
2025-01-22 17:37:14 -0300

    creating txt archive

--- fatec/Intermediário/capítulos/capitulo-11/arquivos-write.py
+++ fatec/Intermediário/capítulos/capitulo-11/arquivos-write.py
@@ -5,7 +5,7 @@ arquivo = open('saida_er_11.1.txt', 'w') #O w é para indicar que será utilizad
 
 num = int(input('Digite um número. Caso o número for igual a 0 o programa será encerrado.'))
 while num != 0:
-    arquivo.write(f'{num}') # É necessário gravar como string, por isso está sendo utilizando o f{}
+    arquivo.write(f'{num}\n') # É necessário gravar como string, por isso está sendo utilizando o f{}
     num = int(input('Digite um número. Caso o número for igual a 0 o programa será encerrado.'))
 arquivo.close()
 
