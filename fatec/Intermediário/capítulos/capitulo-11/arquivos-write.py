7ee4b888e217c814837231204462c6c12d08917c
Roberto <oliveirarobertw@gmail.com>
2025-01-22 17:35:05 -0300

    ex1

---
+++ fatec/Intermediário/capítulos/capitulo-11/arquivos-write.py
@@ -0,0 +1,12 @@
+'''Enunciado 
+Escreva um programa que permaneça em laço lendo números inteiros até que seja digitado 0. Todos os valores digitados, exceto o zero, devem ser gravados em um arquivo em disco, um por linha. Usar o método .write()'''
+
+arquivo = open('saida_er_11.1.txt', 'w') #O w é para indicar que será utilizado para gravação
+
+num = int(input('Digite um número. Caso o número for igual a 0 o programa será encerrado.'))
+while num != 0:
+    arquivo.write(num)
+    num = int(input('Digite um número. Caso o número for igual a 0 o programa será encerrado.'))
+    
+arquivo.close()
+print('\n Fim do Programa')
