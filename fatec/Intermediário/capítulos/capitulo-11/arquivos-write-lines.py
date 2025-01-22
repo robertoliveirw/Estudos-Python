ca6d5f0bd175f297a2acd3765d25553147094cfc
Roberto <oliveirarobertw@gmail.com>
2025-01-22 18:19:43 -0300

    Update arquivos-write-lines.py

--- fatec/Intermediário/capítulos/capitulo-11/arquivos-write-lines.py
+++ fatec/Intermediário/capítulos/capitulo-11/arquivos-write-lines.py
@@ -1,2 +1,3 @@
 '''Enunciado 
 Escreva um programa que permaneça em laço lendo números inteiros até que seja digitado 0. Todos os valores digitados, exceto o zero, devem ser gravados em um arquivo em disco, um por linha. Usar o método .write()'''
+
