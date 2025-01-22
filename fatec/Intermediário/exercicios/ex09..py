305ef2b1eed5eba44acf8416b26652cef4782670
Roberto <oliveirarobertw@gmail.com>
2025-01-22 19:24:35 -0300

    ex09 resolved

--- fatec/Intermediário/exercicios/ex09..py
+++ fatec/Intermediário/exercicios/ex09..py
@@ -1,4 +1,15 @@
 '''Exercício Resolvido 11.3
 Escreva um programa que permaneça em laço lendo números reais até que seja digitado 0. Todos os valores digitados, exceto o zero, devem ser gravados em um arquivo em disco, um por linha, com 3 casas decimais. 
-Usar o método . write ()'''
+Usar o método . writelines ()'''
 
+lst = []
+N = float(input('Digite um número real: '))
+arq = open('ex_09_ex_resolvido_11.3.txt', 'w')
+
+while N != 0:
+    lst.append(f'{N:.3f} \n')
+    N = float(input('Digite um número real: '))
+
+arq.writelines(lst)
+arq.close()
+print('\n Fim do programa')
