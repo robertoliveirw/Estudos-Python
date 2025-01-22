eeb7bcf9f591989caa9f8f36e1bdb44df9035722
Roberto <oliveirarobertw@gmail.com>
2025-01-22 19:25:41 -0300

    Create ex10.py

---
+++ fatec/Intermediário/exercicios/ex10.py
@@ -0,0 +1,4 @@
+'''Exercício Resolvido 11.4
+Escreva um programa que permaneça que leia um arquivo de entrada, sabendo que esse arquivo tem um número inteiro em cada linha. Todos os números lidos devem ser mostrados na tela. Mostrar também a soma dos valores, a quantidade, a média aritmética, o menor valor e o maior valor
+Usar um laço while e na leitura usar o método .readline()'''
+
