40e1442e47cfa45c8fb80d658814ed4b39b22c94
Roberto <oliveirarobertw@gmail.com>
2025-01-22 18:26:52 -0300

    add new archive from exercise

--- fatec/Intermediário/capítulos/capitulo-11/arquivos-write-lines.py
+++ fatec/Intermediário/capítulos/capitulo-11/arquivos-write-lines.py
@@ -1,3 +1,17 @@
 '''Enunciado 
 Escreva um programa que permaneça em laço lendo números inteiros até que seja digitado 0. Todos os valores digitados, exceto o zero, devem ser gravados em um arquivo em disco, um por linha. Usar o método .write()'''
 
+lista = []
+
+arquivo = open('arquivo_ex_write_lines.txt', 'w')
+
+X = float(input('Digite um número:'))
+
+while X != 0:
+    lista.append(f'{X:.3f}\n')
+    X = float(input('Digite um número:'))
+
+arquivo.writelines(lista)
+arquivo.close()
+
+print('\n Fim do programa')
