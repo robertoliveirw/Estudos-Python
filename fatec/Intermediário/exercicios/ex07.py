cd0bc60ff4627575a5829b8ded90f541f21062fc
Roberto <oliveirarobertw@gmail.com>
2025-01-22 19:12:06 -0300

    Create ex07.py

---
+++ fatec/Intermediário/exercicios/ex07.py
@@ -0,0 +1,13 @@
+'''Exercício Resolvido 11.1
+Escreva um programa que permaneça em laço lendo números inteiros até que seja digitado 0. Todos os valores digitados, exceto o zero, devem ser gravados em um arquivo em disco, um por linha.
+Usar o método .write()'''
+
+N = int(input('Digite um número: \n'))
+arq = open('ex_07_ex_resolvido_11.1.txt', 'w')
+
+while N != 0:
+    arq.write(f'{arq} \n')
+    N = int(input('Digite um número: \n'))
+
+arq.close()
+print('\n Fim do programa')
