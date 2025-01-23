2100ab645470ccce9a60ca97598aea4308f5a9ff
Roberto <oliveirarobertw@gmail.com>
2025-01-22 21:24:48 -0300

    Update funcoes.py

---
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes.py
@@ -0,0 +1,16 @@
+# Exemplo 12.1
+
+# Função Criada
+def exibir_hifens():
+    print('___')   
+
+
+
+# Código Principal
+LISTA = [10,20,30,40,50]
+
+for ITEM in LISTA:
+    print(ITEM)
+    exibir_hifens()
+
+print('\n Fim do Programa') 
