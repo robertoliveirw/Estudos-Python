73e6604f58218ac652f1ba623b7e336de3313ba5
Roberto <oliveirarobertw@gmail.com>
2025-01-22 21:32:49 -0300

    Update funcoes_exemplo_12.2 copy.py

--- fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.2 copy.py
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.2 copy.py
@@ -1,7 +1,11 @@
-# Exemplo 12.1
+# Exemplo 12.2
 
 # Função Criada
-
+def lerinteiro():
+    n = int(input('Digite um número: '))
+    return n
 
 
 # Código Principal
+valor = lerinteiro()
+print(f'O valor lido com o uso da função é {valor}')            
