a5670dff9ff0e407d85d23ee9aeb6f49cfacd8db
Roberto <oliveirarobertw@gmail.com>
2025-01-23 13:43:29 -0300

    Update funcoes_exemplo_12.11.py

--- fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.11.py
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.11.py
@@ -1,2 +1,13 @@
 # Exemplo 12.10
 '''Desempacotamento de dados com tupla'''
+# Função Criada
+
+def desempacotamento (a,b,c):
+    return (a + b) / c
+
+# Código Principal
+
+L1 = [12, 20, 5] # Neste caso funciona pq a quantidade de itens na lista é a mesma que foi criada na função.
+A = desempacotamento(*L1)
+print(A)
+
