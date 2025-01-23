ec8ea4a4f86dae198a6f524e72df3aec22674091
Roberto <oliveirarobertw@gmail.com>
2025-01-23 11:45:00 -0300

    Update funcoes_exemplo_12.7.py

--- fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.7.py
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.7.py
@@ -1,8 +1,13 @@
 # Exemplo 12.7
-
+'''Parametro com valor padrão'''
 
 # Função Criada
-
-
+def saudacao(nome, mensagem = 'Olá'):
+    print(mensagem, nome)
 
 # Código Principal
+saudacao = ('Roberto', 'Boa tarde')
+print(saudacao)
+
+saudacao = ('Roberto')
+print(saudacao)
