f316f31d6874a23c7034eb14eea5ba2f407cfb28
Roberto <oliveirarobertw@gmail.com>
2025-01-23 11:58:03 -0300

    Update funcoes_exemplo_12.9.py

--- fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.9.py
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.9.py
@@ -1,3 +1,15 @@
 # Exemplo 12.9
 '''Empacotamento de dados com tupla'''
 
+from random import randint
+
+# Função Criada
+def somatorio (*dados): # O * Indica que é uma tupla
+    r = 0 
+    for i in dados:
+        r += i
+    return r
+
+# Código principal
+v = somatorio(1,2,3,4,5)
+print(v)
