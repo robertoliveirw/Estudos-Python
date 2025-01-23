b8a5d8a574e6b673e65980ee52e86ccbe85f8cb2
Roberto <oliveirarobertw@gmail.com>
2025-01-22 21:43:16 -0300

    Update funcoes_exemplo_12.3.py

--- fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.3.py
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.3.py
@@ -1,4 +1,15 @@
 # Exemplo 12.3
 
+from random import randint
+
 # Função Criada
- 
+def geradordelistas():
+    L = []
+    for i in range(10):
+        L.append(randint(1,10000))
+    return L
+
+# Código Principal
+valores = geradordelistas()
+print(f'Lista gerada >> {valores}')
+
