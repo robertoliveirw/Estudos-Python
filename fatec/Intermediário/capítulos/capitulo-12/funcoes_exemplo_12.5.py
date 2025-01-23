356b7c2d00edf8c1a357244bcfe97e74cc006c19
Roberto <oliveirarobertw@gmail.com>
2025-01-23 11:13:09 -0300

    Update funcoes_exemplo_12.5.py

--- fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.5.py
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.5.py
@@ -1,8 +1,16 @@
 # Exemplo 12.4
-
+from random import randint
 
 # Função Criada
-
+def geradordelistas(qtd):
+    '''Carrega uma lista com QTD numeros inteiros aleatórios'''
+    L = []
+    for i in range(qtd):
+        L.append(randint(1,10000))
+    return L
     
 
 # Código Principal
+valores = int(input('Digite a quantidade de itens na lista: '))
+lista = geradordelistas(valores)
+print(f'Lista gerada >> {lista}')
