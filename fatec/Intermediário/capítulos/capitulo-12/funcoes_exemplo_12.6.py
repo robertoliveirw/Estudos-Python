d448645eca6268d61a6057cce98c30b8a0e7f4e7
Roberto <oliveirarobertw@gmail.com>
2025-01-23 11:22:55 -0300

    Create funcoes_exemplo_12.6.py

---
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.6.py
@@ -0,0 +1,17 @@
+# Exemplo 12.4
+from random import randint
+
+# Função Criada
+def geradordelistas(qtd, a, b):
+    '''Carrega uma lista com uma QTD aleatório e os parâmetros entre o número mínimo e máximo fornecido pelo usuário.'''
+    L = []
+    for i in range(qtd):
+        L.append(randint(a,b))
+    return L
+
+# Código Principal
+valores = int(input('Digite a quantidade de itens na lista: '))
+lmin = int(input('Qual o intervalo inferior da lista: '))
+lmax = int(input('Qual o intervalo superior da lista: '))
+lista = geradordelistas(valores, lmin, lmax)
+print(f'Lista gerada >> {lista}')
