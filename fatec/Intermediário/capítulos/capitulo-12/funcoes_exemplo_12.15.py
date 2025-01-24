2eb279adfddcb5697eeee9647ebfb24c6c87f65a
Roberto <oliveirarobertw@gmail.com>
2025-01-23 21:30:23 -0300

    Update funcoes_exemplo_12.15.py

--- fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.15.py
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.15.py
@@ -1,2 +1,9 @@
 # Exemplo 12.10
 '''Função  Recursiva (Chamar ela mesmo dentro dela)'''
+
+# Função Criada
+def Fatorial(N):
+        if N <= 1:
+            return 1 
+        else:
+              return N * Fatorial(N-1)
