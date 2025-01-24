1c0a252163d373a5f79b2495650dc6061ce295a5
Roberto <oliveirarobertw@gmail.com>
2025-01-24 09:56:10 -0300

    Create funcoes_exemplo_12.19 copy.py

---
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.19 copy.py
@@ -0,0 +1,3 @@
+# Exemplo 12.21
+'''Função  filter -  função filter em Python é usada para filtrar elementos de um iterável (como uma lista, tupla ou conjunto) com base em uma função que retorna True ou False. Apenas os elementos que satisfazem a condição (isto é, onde a função retorna True) serão incluídos no resultado.. '''
+
