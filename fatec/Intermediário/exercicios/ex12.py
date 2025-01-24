1b588ddf82b30e62dff9b986697866360e3eddef
Roberto <oliveirarobertw@gmail.com>
2025-01-23 21:39:22 -0300

    Update ex12.py

--- fatec/Intermediário/exercicios/ex12.py
+++ fatec/Intermediário/exercicios/ex12.py
@@ -1,14 +1,16 @@
 '''Exercício Resolvido 12.8
 Escreva um programa que utilize uma função recursiva para realizar uma contagem regressiva. Este programa deve ler do teclado um inteiro que representa a quantidade de toques dessa contagem regressiva. Quando a contagem chegar em zero o programa deve exibir na tela a mensagem "NO AR!!!" '''
 
+from time import sleep
+
 def Contagem(cont):
     if cont == 0:
         print("NO AR!!!")
     else:   
         print(cont)
+        sleep(1)
         Contagem(cont-1)
 
 toques = int(input('Digite a quantidade de toques da contagem: '))
 print(f'Atenção para o toque de {toques} segundos...')
-print(f'Atenção para o toque de {toques} segundos...')
-print(Contagem(toques))
+Contagem(toques)
