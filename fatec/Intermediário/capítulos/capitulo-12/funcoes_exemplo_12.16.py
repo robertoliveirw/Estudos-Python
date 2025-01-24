89a3795baf57488adb4686def4229386c973f541
Roberto <oliveirarobertw@gmail.com>
2025-01-24 09:25:34 -0300

    Update funcoes_exemplo_12.16.py

--- fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.16.py
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.16.py
@@ -1,14 +1,3 @@
 # Exemplo 12.16
-'''Função  Lambda'''
-
-# Função Criada
-def Fatorial(N):
-        if N <= 1:
-            return 1 
-        else:
-              return N * Fatorial(N-1)
-# Código Principal
-
-entrada = int(input('Digite um número:'))
-F = Fatorial(entrada)
-print(f'O fatorial de {entrada} é {F}')
+'''Função  Lambda
+Uma função lambda de Python é uma função pequena e anônima que pode receber um ou mais parâmetros e seu código se constitui de uma única expressão. '''
