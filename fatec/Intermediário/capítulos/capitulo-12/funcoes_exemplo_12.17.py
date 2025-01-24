160b3d4882eaafb41504e205659c245a6fb341c3
Roberto <oliveirarobertw@gmail.com>
2025-01-24 09:34:38 -0300

    Update funcoes_exemplo_12.17.py

--- fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.17.py
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.17.py
@@ -1,4 +1,8 @@
 # Exemplo 12.17
 '''Função  Lambda - Atribuido ao objeto'''
 
-media = lambda n1, n2, n3: (n1 + n2 + n2)/3
+media = lambda n1, n2, n3: (n1 + n2 + n3) / 3
+
+resultado = media(7, 8, 9)
+
+print(resultado)
