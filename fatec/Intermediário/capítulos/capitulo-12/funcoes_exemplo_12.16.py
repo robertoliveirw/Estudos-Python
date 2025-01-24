6358d505390986f2174d1f68fd709ca0603feebb
Roberto <oliveirarobertw@gmail.com>
2025-01-24 09:29:42 -0300

    Update funcoes_exemplo_12.16.py

--- fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.16.py
+++ fatec/Intermediário/capítulos/capitulo-12/funcoes_exemplo_12.16.py
@@ -1,3 +1,8 @@
 # Exemplo 12.16
 '''Função  Lambda
-Uma função lambda de Python é uma função pequena e anônima que pode receber um ou mais parâmetros e seu código se constitui de uma única expressão. '''
+Uma função lambda de Python é uma função pequena e anônima que pode receber um ou mais parâmetros e seu código se constitui de uma única expressão. ''' 
+
+a = lambda x: x + 2  # Define uma função lambda que adiciona 2 ao valor recebido
+
+resultado = a(5) 
+print(resultado)
