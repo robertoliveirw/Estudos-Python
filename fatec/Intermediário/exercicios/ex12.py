f978a112b099d64aa3f4b97c0431dbde4801a842
Roberto <oliveirarobertw@gmail.com>
2025-01-23 21:34:25 -0300

    Update ex12.py

---
+++ fatec/Intermediário/exercicios/ex12.py
@@ -0,0 +1,3 @@
+'''Exercício Resolvido 12.8
+Escreva um programa que utilize uma função recursiva para realizar uma contagem regressiva. Este programa deve ler do teclado um inteiro que representa a quantidade de toques dessa contagem regressiva. Quando a contagem chegar em zero o programa deve exibir na tela a mensagem "NO AR!!!" '''
+
