93d10a520521d64a0229382c9c128cfd11ba10a3
Roberto <oliveirarobertw@gmail.com>
2025-01-27 18:01:45 -0300

    ex 53 finished

--- cursos/curso-em-video/mundo-02/exercicios/ex53.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex53.py
@@ -1,1 +1,11 @@
 '''Ler uma frase e dizer se ela é um palindromo'''
+
+frase = str(input('Digite a palavra: ')).strip().upper()
+palavra = frase.split()
+junto = ''.join(palavra)
+print(junto)
+inverso = junto[::-1]
+if junto == inverso:
+    print('É palindromo')
+else:
+    print('Não é palindromo')
