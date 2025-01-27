a81bfa8460eb09f31f5e641d995bea8620bc851a
Roberto <oliveirarobertw@gmail.com>
2025-01-27 15:47:06 -0300

    ex36 completed

--- cursos/curso-em-video/mundo-02/exercicios/ex36.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex36.py
@@ -1,6 +1,18 @@
 '''Empréstimo Bancário
-Valor da Casa
-Salário
-Quantos anos vai pagar
+Valor da Casa OK
+Salário OK
+Quantos anos vai pagar OK
 
 Pretação mensal não pode exceder 30% do salário ou então o empréstimo será negado'''
+print('Calculadora de Financiamento')
+
+vcasa = float(input('Qual o valor da casa? '))
+salario = float(input('Qual o seu salário? '))
+anos = int(input('Em quantos anos deseja pagar? '))
+mensalidade = (vcasa/anos)/12
+prestacao = salario*0.3
+
+if prestacao <= mensalidade:
+    print('Financiamento Rejeitado!')
+else:
+    print('Aprovado')
