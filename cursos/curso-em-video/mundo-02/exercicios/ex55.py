265368faff7c7d490b1393b0d0b288564ca72495
Roberto <oliveirarobertw@gmail.com>
2025-01-27 18:08:14 -0300

    ex55 - finished

--- cursos/curso-em-video/mundo-02/exercicios/ex55.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex55.py
@@ -1,1 +1,12 @@
 '''Ler 5 números e falar o maior e o menor'''
+
+lista = []
+
+for c in range(0,5):
+    peso = float(input('Digite o peso: '))
+    lista.append(peso)
+
+maior = max(lista)
+menor = min(lista)
+
+print(f'O maior número é {maior} e o menor é {menor}')
