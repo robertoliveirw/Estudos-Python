d9d1685b16450973437d46a19778b4b198013686
Roberto <oliveirarobertw@gmail.com>
2025-01-27 15:57:13 -0300

    completed

--- cursos/curso-em-video/mundo-02/exercicios/ex37.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex37.py
@@ -2,3 +2,18 @@
 1 - Binário
 2 - Octal
 3 - Hexadecimal'''
+
+numero = int(input('Digite um número: '))
+escolha = int(input('Você deseja transformar o número inteiro para: 1 - Binário | 2 - Octal | 3 - Hexadecimal:' ))
+
+if escolha == 1:
+    binario = bin(numero)  # Converte para binário
+    print(f'O numero {numero} em Binário é: {binario}')
+elif escolha == 2: 
+    octal = oct(numero)  # Converte para octal
+    print(f'O numero {numero} em Octal é: {octal}')
+elif escolha == 3:
+        hexadecimal = hex(numero)
+        print(f'O número {numero} em Hexadecimal é: {hexadecimal}')
+else:
+        print("Opção inválida. Por favor, escolha 1, 2 ou 3.")
