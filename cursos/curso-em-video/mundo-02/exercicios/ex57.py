e45c7d8e648aa01eeb4027d369e0489a99326519
Roberto <oliveirarobertw@gmail.com>
2025-01-27 20:57:57 -0300

    ex57 finished

--- cursos/curso-em-video/mundo-02/exercicios/ex57.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex57.py
@@ -1,1 +1,12 @@
 '''Fazer um programa que só aceite M ou F para o sexo'''
+
+sexo = 'string aleatória'
+
+while sexo not in 'MnFf':
+    sexo = str(input('Digite o sexo [M/F]')).upper().strip()  
+    if sexo == 'M':
+        print('Seu sexo é Masculino')
+    elif sexo == 'F':
+        print('Seu sexo é Feminino')
+    else:
+        print('Escolha inválida!')
