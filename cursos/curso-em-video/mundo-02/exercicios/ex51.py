339bc1994680b551c014234c35748b50b8b40de4
Roberto <oliveirarobertw@gmail.com>
2025-01-27 17:58:43 -0300

    ex51 finished

--- cursos/curso-em-video/mundo-02/exercicios/ex51.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex51.py
@@ -1,1 +1,8 @@
 '''Ler o primeiro termo e a razão de uma PA e motrar os 10 primeiros termos'''
+
+pt = int(input('Digite o início: '))
+razao = int(input('Digite o passo: '))
+decimotermo = pt + (10-1)*razao
+
+for c in range(pt, decimotermo, razao):
+    print(c)
