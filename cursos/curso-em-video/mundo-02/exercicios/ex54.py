773e98987b0a0c8577c2782a330e2e3bb3ba654c
Roberto <oliveirarobertw@gmail.com>
2025-01-27 18:04:58 -0300

    ex54 finished

--- cursos/curso-em-video/mundo-02/exercicios/ex54.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex54.py
@@ -1,1 +1,14 @@
 '''Ler o nome de pessoas e dizer quantas pessoas ainda não atingiram a maioridade e quantas já são de maiores'''
+from datetime import date
+
+atual = date.today().year
+menor = 0
+maior = 0
+for c in range(1, 8):
+    ano = int(input('Digite o ano de nascimento: '))
+    idade = atual - ano
+    if idade < 18:
+        menor += 1
+    else:
+        maior += 1
+print('menores = {}\nmaiores = {}'.format(menor, maior))
