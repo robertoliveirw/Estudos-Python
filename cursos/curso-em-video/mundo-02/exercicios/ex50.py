2d37654c4ebad305116a060b5076bdd9d0726db9
Roberto <oliveirarobertw@gmail.com>
2025-01-27 17:55:19 -0300

    ex50 finished

--- cursos/curso-em-video/mundo-02/exercicios/ex50.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex50.py
@@ -1,1 +1,10 @@
 '''Ler 6 numeros inteiros e mostrar a soma apenas dos numeros pares'''
+
+par = 0
+cont = 0 
+for c in range(0,6):
+    n = int(input('Digite um número: '))
+    if c % 2 == 0:
+        par+= n
+        cont += 1
+print(f'Você informou {cont} números pares, a soma dos números pares é {par}')
