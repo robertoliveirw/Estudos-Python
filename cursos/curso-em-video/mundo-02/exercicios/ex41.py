defb4ff59aa3ff611bb8b81f4fef59b6fc60e5c6
Roberto <oliveirarobertw@gmail.com>
2025-01-27 16:13:51 -0300

    ex41 completed

--- cursos/curso-em-video/mundo-02/exercicios/ex41.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex41.py
@@ -4,3 +4,19 @@ Até 14 anos Infantil
 até 19 anos Junior
 Até 20 anos Senior
 Acima Master'''
+
+idade = int(input("Digite a idade do atleta: "))
+
+# Classificação por idade
+if idade <= 9:
+    categoria = "Mirim"
+elif idade <= 14:
+    categoria = "Infantil"
+elif idade <= 19:
+    categoria = "Junior"
+elif idade == 20:
+    categoria = "Senior"
+else:
+    categoria = "Master"
+
+print(f"O atleta tem {idade} anos e está na categoria: {categoria}")
