5242176523ae6fc91c46a5f8b667ccb4e97f1d4e
Roberto <oliveirarobertw@gmail.com>
2025-01-27 16:29:28 -0300

    ex34 completed

--- cursos/curso-em-video/mundo-02/exercicios/ex42.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex42.py
@@ -2,3 +2,17 @@
 - equilatero 
 - Isosceles
 - Esaleno'''
+
+r1 = float(input("Reta 1: "))
+r2 = float(input("Reta 2: "))
+r3 = float(input("Reta 3: "))
+
+if r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1:
+    if r1 == r2 == r3:
+        print("As três retas podem formar um triângulo EQUILÁTERO.")
+    elif r1 == r2 or r1 == r3 or r2 == r3:
+        print("As três retas podem formar um triângulo ISÓSCELES.")
+    else:
+        print("As três retas podem formar um triângulo ESCALENO.")
+else:
+    print("As três retas NÃO podem formar um triângulo.")
