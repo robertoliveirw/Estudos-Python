e19b7ee30937361cfa84547a49ee2b420b5a6529
Roberto <oliveirarobertw@gmail.com>
2025-01-27 17:46:25 -0300

    Update ex48.py

--- cursos/curso-em-video/mundo-02/exercicios/ex48.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex48.py
@@ -1,2 +1,10 @@
 '''Calcular a soma entre todos os numeros impares que são multiplos de 3 entre 1 e 500'''
 
+n=0
+cont = 0
+
+for c in range (1, 501,2):
+    if c % 3 == 0:
+        cont += 1
+        n+= c
+print(f'A soma de todos os {cont} valores é {n}')
