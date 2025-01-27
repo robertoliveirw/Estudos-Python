da752f41a6f5b620cc6a05f40b55d5fa2cab70db
Roberto <oliveirarobertw@gmail.com>
2025-01-27 16:03:24 -0300

    38 completed

--- cursos/curso-em-video/mundo-02/exercicios/ex38.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex38.py
@@ -1,1 +1,11 @@
 '''Ler dois números inteiros e dizer qual o maior'''
+
+n1 =  int(input('Digite um número: '))
+n2 =  int(input('Digite um número: '))
+
+if n1 > n2:
+    print(f'{n1} é maior do que {n2}')
+elif n2 > n1:
+    print(f'{n2} é maior do que {n1}')
+else: 
+    print('Os números são iguais')
