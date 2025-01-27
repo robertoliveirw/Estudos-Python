88c4bf09b1f60c802cc6aebe8abf05325d0a4557
Roberto <oliveirarobertw@gmail.com>
2025-01-27 15:10:22 -0300

    Update condicoes-aninhadas.py

---
+++ cursos/curso-em-video/mundo-02/condicoes-aninhadas.py
@@ -0,0 +1,7 @@
+# O aninhamento é ocorre quando se utiliza uma estrutura dentro de uma mesma estrutura
+nome = str(input('Qual o seu nome? ')).strip()
+
+if nome == 'Roberto':
+    print(f'Seu nome é muito bonito.\nPrazer em te conhecer, {nome}!')
+else:
+    print(f'Tenha um bom dia, {nome}!')
