55ebd8f0afcde688636689ab108b0defda75eb71
Roberto <oliveirarobertw@gmail.com>
2025-01-27 16:09:49 -0300

    ex39 - completed

--- cursos/curso-em-video/mundo-02/exercicios/ex39.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex39.py
@@ -4,3 +4,15 @@
 - Se já passou o tempo de Alistamento
 
 Deve mostrar o tempo que falta ou o tempo que passou'''
+
+ano = int(input('Qual ano você nasceu? '))
+idade = 2025 - ano
+
+if idade == 18:
+    print('Está na hora de se alistar')
+elif idade < 18:
+    f_idade = 18 - idade
+    print(f'Não está na hora de se alistar! \nFaltam {f_idade} anos.')
+else:
+    f_idade = idade - 18
+    print(f'Você já passou da hora de se alistar! \nEstá {f_idade} anos atrasado!.')
