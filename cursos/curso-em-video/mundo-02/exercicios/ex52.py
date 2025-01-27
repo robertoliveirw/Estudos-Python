0327967febc4c80a4b5fff3b5a8f4505504f6cf4
Roberto <oliveirarobertw@gmail.com>
2025-01-27 18:01:10 -0300

    ex52 finished

--- cursos/curso-em-video/mundo-02/exercicios/ex52.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex52.py
@@ -1,1 +1,14 @@
 '''Ler se o número é primo ou não'''
+n = int(input('verificar primos até: '))
+tot = 0
+for c in range(1, n+1):
+    if n % c == 0:
+        print('\033[33m{}'.format(c), end=' ')
+        tot += 1
+    else:
+        print('\033[31m{}'.format(c), end=' ')
+print('\n\033[mO número {} foi divisível {} vezes.'.format(n, tot))
+if tot == 2:
+    print('Por isso é primo.')
+else:
+    print('Por isso não é primo.')
