44d573b6eb6374c07a0e8cda966f4ae8101f9594
Roberto <oliveirarobertw@gmail.com>
2025-01-27 16:11:38 -0300

    ex40 completed

--- cursos/curso-em-video/mundo-02/exercicios/ex40.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex40.py
@@ -2,3 +2,21 @@
 Abaixo de 5 reprovado
 Entre 5 e 6.9 recuperação
 Acima de 7 aprovado'''
+
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+
+print(f"\nSua média é: {media:.2f}")
+
+
+if media < 5:
+    print("Situação: REPROVADO")
+elif 5 <= media <= 6.9:
+    print("Situação: RECUPERAÇÃO")
+else:  # média >= 7
+    print("Situação: APROVADO")
