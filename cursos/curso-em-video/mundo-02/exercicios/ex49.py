ae8e7d28c031efe4064512e33219827e530ee4fc
Roberto <oliveirarobertw@gmail.com>
2025-01-27 17:51:13 -0300

    ex49 - finished

--- cursos/curso-em-video/mundo-02/exercicios/ex49.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex49.py
@@ -1,1 +1,9 @@
 '''Refazer o ex09 mostrando a taubada de um número que o usuário escolher só que agora usando o laço for'''
+
+# Tabuada
+numero = int(input("Digite um número: \n"))
+print(f"Tabuada do {numero}:")
+
+for c in range(0, 11):
+    nn = numero*c
+    print(f'{numero} x {c} = {nn}')
