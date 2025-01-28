bf7b59ec2e6ac30f7880b7ce0b6f49b727456776
Roberto <oliveirarobertw@gmail.com>
2025-01-27 21:12:51 -0300

    Update ex58.py

--- cursos/curso-em-video/mundo-02/exercicios/ex58.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex58.py
@@ -1,1 +1,39 @@
 '''Aprimorar desafio 28 (numero entre 0 e 10), mas dessa vez pedindo até a pessoa acertar, e no final mostre quantas tentativas foram necessárias'''
+import random
+
+numeroaleatorio = random.randint(0, 10)
+numero = int(input('Em qual número de 0 a 10 eu estou pensando? '))
+cont = 1
+
+while numeroaleatorio != numero: 
+    if numeroaleatorio != numero:
+        print(f'Você errou, tente novamente!')
+        numero = int(input('Em qual número de 0 a 10 eu estou pensando? '))
+        cont += 1
+        
+print('Você acertou!')
+print(f'Foram necessárias {cont} tentativas até acertar!')
+
+continuar = int(input('Deseja continuar: [1] SIM ou [2] NÃO?'))
+
+if continuar == 1: 
+    
+    numeroaleatorio = random.randint(0, 10)
+    numero = int(input('Em qual número de 0 a 10 eu estou pensando? '))
+    cont = 1
+    while numeroaleatorio != numero: 
+        if numeroaleatorio != numero:
+            print(f'Você errou, tente novamente!')
+            numero = int(input('Em qual número de 0 a 10 eu estou pensando? '))
+            cont += 1
+        
+    print('Você acertou!')
+    print(f'Foram necessárias {cont} tentativas até acertar!')
+
+elif continuar == 2:
+    
+    print('Muito obrigado por jogar!\nAté a próxima.')
+
+else:
+    
+    print('Resposta inválida!')
