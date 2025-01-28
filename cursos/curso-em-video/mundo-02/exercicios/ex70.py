aa9cb3b6f92e793b806a67e9bc4428f060a35d35
Roberto <oliveirarobertw@gmail.com>
2025-01-28 13:28:09 -0300

    Create ex70.py

---
+++ cursos/curso-em-video/mundo-02/exercicios/ex70.py
@@ -0,0 +1,6 @@
+'''Ler nome e preço de vários produtos. Perguntar se quer continuar add dados. 
+
+Total Gasto na compra
+Produtos que custam mais de 1000
+Produto mais barato (retornar o nome)'''
+
