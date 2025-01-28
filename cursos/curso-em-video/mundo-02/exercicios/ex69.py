99d69e8ee0e6f35ff33a4c3ba90935b91fc30b2a
Roberto <oliveirarobertw@gmail.com>
2025-01-28 13:25:34 -0300

    Update ex69.py

--- cursos/curso-em-video/mundo-02/exercicios/ex69.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex69.py
@@ -1,2 +1,6 @@
-'''Jogar par ou ímpar. Só será interrompido quando o jogador perder, mostrando a quantidade de vitórias consecutivas que ele conquistou no final do jogo'''
+'''Ler idade e o sexo de várias pessoas. A cada pessoa cadastrada o programa deve perguntar se o usuário quer ou nao continuar. Mostrar no final:
+
+Pessoas com mais de 18 anos
+Quantas mulheres cadastradas
+Quantas mulheres tem menos de 20 anos'''
 
