f6fc83aa1c0d637c03bebae66dc6ec04f575c234
Roberto <oliveirarobertw@gmail.com>
2025-01-27 21:50:49 -0300

    ex64 finished

--- cursos/curso-em-video/mundo-02/exercicios/ex64.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex64.py
@@ -1,1 +1,11 @@
 '''Ler vários números inteiros e parar com 999. No final mostrar a soma entre eles e desconsiderando o 999'''
+
+c = 0
+num = 0
+while num != 999:
+    num = int(input('Digite um número: '))
+    c += num
+
+nnum = c - 999
+
+print(nnum)
