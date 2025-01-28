92530f4ed4044677989522d820953d7b2d24ff99
Roberto <oliveirarobertw@gmail.com>
2025-01-27 21:13:54 -0300

    Update ex58.py

--- cursos/curso-em-video/mundo-02/exercicios/ex58.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex58.py
@@ -14,26 +14,3 @@ while numeroaleatorio != numero:
 print('Você acertou!')
 print(f'Foram necessárias {cont} tentativas até acertar!')
 
-continuar = int(input('Deseja continuar: [1] SIM ou [2] NÃO?'))
-
-if continuar == 1: 
-    
-    numeroaleatorio = random.randint(0, 10)
-    numero = int(input('Em qual número de 0 a 10 eu estou pensando? '))
-    cont = 1
-    while numeroaleatorio != numero: 
-        if numeroaleatorio != numero:
-            print(f'Você errou, tente novamente!')
-            numero = int(input('Em qual número de 0 a 10 eu estou pensando? '))
-            cont += 1
-        
-    print('Você acertou!')
-    print(f'Foram necessárias {cont} tentativas até acertar!')
-
-elif continuar == 2:
-    
-    print('Muito obrigado por jogar!\nAté a próxima.')
-
-else:
-    
-    print('Resposta inválida!')
