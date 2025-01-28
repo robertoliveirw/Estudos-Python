16840789c4a9ffe1a75f2230e7a2e06472982ad5
Roberto <oliveirarobertw@gmail.com>
2025-01-27 21:30:25 -0300

    Update ex60.py

--- cursos/curso-em-video/mundo-02/exercicios/ex60.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex60.py
@@ -1,1 +1,13 @@
 '''Ler um número e calcular o fatorial'''
+
+n = int(input('Digite um número para obter o fatorial: '))
+c = n
+f = 1
+while c > 0:
+    if c != 1:
+        print(f'{c} x')
+    else:
+        print('{c} =')
+    f *= c
+    c -= 1
+print(f'{f}.')
