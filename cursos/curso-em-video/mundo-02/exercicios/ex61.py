8ed35d2132da6f4d3051f6ae413be583e63dc534
Roberto <oliveirarobertw@gmail.com>
2025-01-27 21:34:17 -0300

    Update ex61.py

--- cursos/curso-em-video/mundo-02/exercicios/ex61.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex61.py
@@ -1,1 +1,18 @@
 '''Fazer o desafio 51 com while'''
+
+'''pt = int(input('Digite o início: '))
+razao = int(input('Digite o passo: '))
+decimotermo = pt + (10-1)*razao
+
+for c in range(pt, decimotermo, razao):
+    print(c)'''
+
+p = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite o passo: '))
+termo = p
+c = 1
+while c <= 10:
+    print(f'{termo} ➡ ')
+    termo += razao
+    c += 1
+print('FIM')
