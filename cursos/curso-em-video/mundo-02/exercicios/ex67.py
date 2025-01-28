ded153f07a223fcef5d7943b217d47a7e5539279
Roberto <oliveirarobertw@gmail.com>
2025-01-28 13:34:50 -0300

    ex67 finished

--- cursos/curso-em-video/mundo-02/exercicios/ex67.py
+++ cursos/curso-em-video/mundo-02/exercicios/ex67.py
@@ -1,2 +1,12 @@
 '''Faça um programa que mostre a tabuada de vários números um de cada vez para cada valor digitado. O programa será interrmopido quando um número negativo for solicitado'''
+while True:
+    numero = int(input("Digite um número: \n"))
+    print(f"Tabuada do {numero}:")
+    
+    if numero < 0:
+        print('Programa encerrado!')
+        break
 
+    for c in range(0, 11):
+        nn = numero*c
+        print(f'{numero} x {c} = {nn}')
