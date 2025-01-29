227ed1f651fa4b76c13e9a44952bcaae275a76d5
Roberto <oliveirarobertw@gmail.com>
2025-01-29 14:04:39 -0300

    Create excecoes.py

---
+++ cursos/fatec/02.Intermediário/capítulos/capitulo-14/excecoes.py
@@ -0,0 +1,4 @@
+''' 
+- Tratamento de exceção – é o que consideramos no nosso programa quando existe a possibilidade de ocorrência de erros em um trecho de código e que precisamos tratar para que o nosso programa não tenha sua execução interrompida. Este aspecto foi tratado no capítulo 6 do módulo básico;
+- Levantamento de exceção – é o que precisamos implementar quando estamos escrevendo um código, uma situação de erro pode ocorrer e queremos gerar uma exceção para ser tratada em outro ponto do programa. Este é o assunto deste capítulo.
+'''
