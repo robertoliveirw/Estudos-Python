36a2d872eadb2979f2525a9601c60544f71a3c6a
Roberto <oliveirarobertw@gmail.com>
2025-03-05 11:29:56 -0300

    Update ex06.py

--- exercícios/ex06.py
+++ exercícios/ex06.py
@@ -1,1 +1,10 @@
 # Calcule o fatorial de um número informado pelo usuário usando um loop.
+
+numero = int(input('Digite um número:'))
+controlador = 1
+
+for numeros in range(1, numero+1):
+    print(numeros)
+    controlador *= numeros
+    
+print(f'A soma é: {controlador}')
