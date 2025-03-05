a851d4659cea47469236111f5a1a4b4d4b59b269
Roberto <oliveirarobertw@gmail.com>
2025-03-05 10:58:04 -0300

    Update ex03.py

--- exercícios/ex03.py
+++ exercícios/ex03.py
@@ -1,1 +1,11 @@
 # Leia dois números e realize as operações básicas (subtração, multiplicação e divisão).
+
+numero1 = int(input('Digite um número:'))
+numero2 = int(input('Digite um número:'))
+
+soma = numero1 + numero2
+sub = numero1 - numero2
+mult = numero1 * numero2
+div = numero1 / numero2
+
+print(f'A soma, subtração, multiplicão e divisão, respectivamente são: {soma}, {sub}, {mult}, {div}')
