9a1b45d195e15ac73331033cf30dc16564715662
Roberto <oliveirarobertw@gmail.com>
2025-03-05 12:36:20 -0300

    ex10

--- exercícios/ex10.py
+++ exercícios/ex10.py
@@ -1,1 +1,6 @@
 # Crie um programa que receba uma string e a imprima de forma invertida.
+
+string = input('Digite algo para ser invertido:')
+
+string_invertida = string[::-1]
+print(string_invertida)
