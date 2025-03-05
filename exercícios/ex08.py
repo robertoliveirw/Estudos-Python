1e07ed65d2e5cb635a2184e611a0934a27c6bc39
Roberto <oliveirarobertw@gmail.com>
2025-03-05 12:30:31 -0300

    ex08

--- exercícios/ex08.py
+++ exercícios/ex08.py
@@ -1,1 +1,13 @@
-# Verifique se um número fornecido pelo usuário é primo.
+number = int(input('Numero: '))
+
+if number >= 1:
+    for i in range(1, number):
+        if number % i != 0:
+            print(number, 'é primo')
+        else:
+            print(number, 'não é primo')
+            break
+elif number == 0:
+    print(number, 'é zero')
+else:
+    print(number, 'é negativo')
