1a242742607012341930af478caab149db10cba4
Roberto <oliveirarobertw@gmail.com>
2025-03-05 11:17:14 -0300

    Update ex05.py

--- exercícios/ex05.py
+++ exercícios/ex05.py
@@ -1,1 +1,8 @@
 # Crie um programa que determine se um número digitado pelo usuário é par ou ímpar.
+
+numero = int(input('Digite um número:'))
+
+if numero % 2 == 0: 
+    print('Par')
+else:
+    print('Ímpar') 
