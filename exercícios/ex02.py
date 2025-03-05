304c8658173ebf1ed545fb5cd508e8932f8c8e34
Roberto <oliveirarobertw@gmail.com>
2025-03-05 10:54:46 -0300

    ex2 solved

--- exercícios/ex02.py
+++ exercícios/ex02.py
@@ -1,1 +1,8 @@
 # Peça ao usuário para inserir dois números e exiba a soma deles.
+
+numero1 = int(input('Digite um número:'))
+numero2 = int(input('Digite um número:'))
+
+soma = numero1 + numero2
+
+print(f'A soma do número {numero1} e do número {numero2} é: {soma}')
