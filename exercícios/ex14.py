0eedea4deeb17cebcbaa1649aa9808f4bdfba795
Roberto <oliveirarobertw@gmail.com>
2025-03-05 14:51:56 -0300

    ex14

--- exercícios/ex14.py
+++ exercícios/ex14.py
@@ -1,1 +1,8 @@
 # Receba uma string e retorne-a com todas as letras em maiúsculas, e em seguida em minúsculas.
+
+string = input('Digite uma frase: ')
+string_minuscula = string.lower()
+string_maiuscula = string.upper()
+
+print(string_minuscula)
+print(string_maiuscula)
