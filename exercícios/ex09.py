ccb0343a8d3e48c464c4434212d49356efd980cd
Roberto <oliveirarobertw@gmail.com>
2025-03-05 12:31:57 -0300

    ex09

--- exercícios/ex09.py
+++ exercícios/ex09.py
@@ -1,1 +1,20 @@
 # Gere a sequência de Fibonacci até um número ou quantidade de termos especificada pelo usuário.
+
+print (" Trabalho de Fibonacci ")
+
+n =int( input(" Digite Quantos termos você deseja :"))
+t1 = 0
+t2 = 1
+
+print(f"{t1} . {t2} .",end="")
+
+cont = 6
+
+while cont <= n:
+    t3 = t1 + t2
+    print (f"  {t3}.",end="")
+    t1 = t2
+    t2 = t3
+    cont +=1
+
+print( " Fim ")  
