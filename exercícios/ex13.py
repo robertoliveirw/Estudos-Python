b709f68a1907848ac5adeaaaea9084234562e13e
Roberto <oliveirarobertw@gmail.com>
2025-03-06 10:59:04 -0300

    logic added

--- exercícios/ex13.py
+++ exercícios/ex13.py
@@ -1,1 +1,13 @@
 # Conte quantas vezes cada caractere aparece em uma string (use um dicionário para armazenar os resultados).
+'''
+Lógica: 
+- Pegar a string;
+- Tirar as letras duplicadas;
+- Verificar quantas vezes cada letra aparece
+- Adicionar num dicionário na seguinte ordem:
+    {
+    a: 2x
+    b: 3x
+    c: 2x
+     }
+'''
