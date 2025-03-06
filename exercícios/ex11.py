dd0a3da0cfbc995b900a0c4e6efdb4e5f47758eb
Roberto <oliveirarobertw@gmail.com>
2025-03-06 10:13:46 -0300

    ex11

--- exercícios/ex11.py
+++ exercícios/ex11.py
@@ -1,1 +1,18 @@
 # Verifique se uma string é um palíndromo (lê-se igual de trás para frente).
+
+'''string1 = 'oi'
+string2 = 'io'
+string2_invertida = string2[::-1]
+
+if string2_invertida == string1:
+    print('Palíndromo')
+else:
+    print('Não é palíndromo')'''
+
+string = input('Digite um texto para verificar se é palíndromo:')
+invertida = string[::-1]
+
+if string == invertida:
+    print('Palíndromo')
+else:
+    print('Não é palíndromo') 
