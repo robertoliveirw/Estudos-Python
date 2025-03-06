fd814817665c08cab41ae9988696f5331e275c64
Roberto <oliveirarobertw@gmail.com>
2025-03-06 10:33:23 -0300

    ex12

--- exercícios/ex12.py
+++ exercícios/ex12.py
@@ -1,1 +1,9 @@
 # Escreva uma função que conte o número de vogais em uma frase ou palavra.
+
+# Função
+def contar_vogais(string):
+    vogais = 'AEIOU'
+    return sum(1 for letra in string if letra in vogais)
+
+frase = input('Digite uma frase:').upper()
+print(contar_vogais(frase))
