f6e12245fc3f07b63886fe26ffc417286f13e6b7
Roberto <oliveirarobertw@gmail.com>
2025-03-06 15:16:54 -0300

    ex18

--- exercícios/ex18.py
+++ exercícios/ex18.py
@@ -1,1 +1,19 @@
 # Crie um programa que ordene uma lista de números em ordem crescente.
+from random import randint
+
+# Função 
+
+def geradordelista(qtde, limite_inferior, limite_superior):
+    lista = []
+    for i in  range(qtde):
+        lista.append(randint(limite_inferior, limite_superior))
+    return lista
+
+qtd_valores = int(input('DIGITE A QUANTIDADE DE NÚMEROS DA LISTA:'))
+int_inferior = int(input('DIGITE O INTERVALO INFERIOR DE NÚMEROS DA LISTA:'))
+int_superior = int(input('DIGITE O INTERVALO SUPERIOR DE NÚMEROS DA LISTA:'))
+
+lista_atual = geradordelista(qtd_valores, int_inferior, int_superior)
+lista_ordenada = sorted(lista_atual)
+
+print(f'Lista gerada {lista_atual} \n Lista ordenada {lista_ordenada}')
