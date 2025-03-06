d5440a8b36666ea538f14d22ee2bf1291be9c9c7
Roberto <oliveirarobertw@gmail.com>
2025-03-06 14:39:13 -0300

    ex13

--- exercícios/ex13.py
+++ exercícios/ex13.py
@@ -1,13 +1,21 @@
 # Conte quantas vezes cada caractere aparece em uma string (use um dicionário para armazenar os resultados).
+
 '''
-Lógica: 
-- Pegar a string;
-- Tirar as letras duplicadas;
-- Verificar quantas vezes cada letra aparece
-- Adicionar num dicionário na seguinte ordem:
-    {
-    a: 2x
-    b: 3x
-    c: 2x
-     }
+Lógica:
+- Pegar a string original.
+- Criar um dicionário vazio para armazenar os resultados.
+- Verificar cada caractere da string, um por um:
+    - Se o caractere já está no dicionário, aumente sua contagem em +1.
+    - Se o caractere não está no dicionário, adicione-o com valor inicial 1.
+- Resultado final: Um dicionário onde cada chave é um caractere único, e o valor é quantas vezes ele aparece.
 '''
+string = input('Digite um texto: ')
+dicionario_string = {}
+ 
+for caractere in string: 
+    if caractere in dicionario_string: 
+        dicionario_string[caractere] += 1
+    else:
+        dicionario_string[caractere] = 1
+    
+print(dicionario_string)
