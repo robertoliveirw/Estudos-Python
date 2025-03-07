66c143f57d4dd746c21470d40bd0309f529ff14c
Roberto <oliveirarobertw@gmail.com>
2025-03-07 16:53:57 -0300

    ex19 solved

--- exercícios/ex19.py
+++ exercícios/ex19.py
@@ -1,1 +1,17 @@
 # Dada uma lista com números (ou strings), remova os elementos duplicados.
+
+from random import randint
+
+def geradordelista(qtde):
+    L = []
+    for i in range(qtde):
+        L.append(randint(1, 5))
+    return L
+
+qtde_de_itens = int(input('QUANTIDADE DE NÚMEROS DA LISTA'))
+
+lista_original = geradordelista(qtde_de_itens)
+print('A lista original é: ', lista_original)
+
+lista_sem_duplicados = sorted(set(lista_original))
+print('A lista sem duplicados é: ', lista_sem_duplicados)
