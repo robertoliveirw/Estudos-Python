d74d3dbc48ada479b7fe42fec208e79158934683
Roberto <oliveirarobertw@gmail.com>
2025-03-07 17:07:25 -0300

    ex20 solved

--- exercícios/ex20.py
+++ exercícios/ex20.py
@@ -1,1 +1,16 @@
 # Inverta os elementos de uma lista sem usar métodos prontos.
+from random import randint
+
+def geradordelista(qtde):
+    L = []
+    for i in range(qtde):
+        L.append(randint(1, 10))
+    return L
+
+qtde_de_itens = int(input('QUANTIDADE DE NÚMEROS DA LISTA: '))
+
+lista_original = geradordelista(qtde_de_itens)
+lista_invertida = lista_original[::-1]
+
+print('A lista original é: ', lista_original)
+print('A lista invertida é: ', lista_invertida)
