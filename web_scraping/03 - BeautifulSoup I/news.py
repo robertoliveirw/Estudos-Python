db570c234437c12cfeb3b9bf6ac6154bb01fcba9
Roberto <oliveirarobertw@gmail.com>
2025-03-17 22:52:05 -0300

    in progress

---
+++ web_scraping/03 - BeautifulSoup I/news.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://g1.globo.com/')
+content = response.content
+site = BeautifulSoup(content, 'html.parser')
+print(type(site))
