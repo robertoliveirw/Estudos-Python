f99119cf5634057f3d474adf3e823dffaf24228b
Roberto <oliveirarobertw@gmail.com>
2025-03-20 22:23:17 -0300

    aprimoramento

--- Projetos/bot_senhas/main.py
+++ Projetos/bot_senhas/main.py
@@ -3,23 +3,23 @@ import csv
 def tratar_string(texto):
     palavras = texto.split()
     
-    texto1 = palavras[0] if len(palavras) > 0 else ""
-    texto2 = palavras[1] if len(palavras) > 1 else ""
-    texto3 = palavras[2] if len(palavras) > 2 else ""
-    texto4 = palavras[3] if len(palavras) > 3 else ""
+    aplicacao = palavras[0] if len(palavras) > 0 else ""
+    usuario = palavras[1] if len(palavras) > 1 else ""
+    email = palavras[2] if len(palavras) > 2 else ""
+    senha = palavras[3] if len(palavras) > 3 else ""
     
-    return texto1, texto2, texto3, texto4
+    return aplicacao, usuario, email, senha
 
-def escreve_csv(texto1, texto2, texto3, texto4):
+def escreve_csv(aplicacao, usuario, email, senha):
     with open('database/initial_database.csv','a', newline='') as csvfile:
         escrever = csv.writer(csvfile, delimiter=';')
-        escrever.writerow([texto1, texto2, texto3, texto4])
+        escrever.writerow([aplicacao, usuario, email, senha])
 
-def procura_csv(aplicacao):
+def procura_csv(aplicacao_desejada):
     with open('database/initial_database.csv', newline='') as f:
         reader = csv.reader(f, delimiter=';')
         for linha in reader:
-            if linha and aplicacao in linha[0]: 
-                app, usuario, email, senha = linha[:4] 
-                return f"Aplicativo: {app}\nUsuário: {usuario}\nE-mail: {email}\nSenha: {senha}"
-    return f"Nenhum aplicativo encontrado para: {aplicacao}"
+            if linha and aplicacao_desejada in linha[0]: 
+                aplicacao, usuario, email, senha = linha[:4] 
+                return f"Aplicativo: {aplicacao}\nUsuário: {usuario}\nE-mail: {email}\nSenha: {senha}"
+    return f"Nenhum aplicativo encontrado para: {aplicacao_desejada}"
