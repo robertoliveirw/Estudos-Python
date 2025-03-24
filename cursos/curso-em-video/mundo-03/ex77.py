85b3b7a2bdc4f553d0add892eef1ecf94db03b9c
Roberto <oliveirarobertw@gmail.com>
2025-03-24 15:33:41 -0300

    Update ex77.py

--- cursos/curso-em-video/mundo-03/ex77.py
+++ cursos/curso-em-video/mundo-03/ex77.py
@@ -1,3 +1,18 @@
 '''
 Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar para cada palavra quais são as suas vogais
 '''
+
+tupla = (
+        'Amaciante',
+        'Costa',
+        'Peito'
+        )
+
+vogais = 'AEIOU'
+for palavra in tupla:
+    palavra = palavra.upper()
+    vogais_palavra = ()
+    for letra in palavra:
+        if letra in vogais:
+            vogais_palavra += (letra,)  
+    print(f'A palavra {palavra} possui as vogais: {", ".join(vogais_palavra)}')
