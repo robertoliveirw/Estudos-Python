b07512421b1f6be8d9c7798b285c857769a848c2
Roberto <oliveirarobertw@gmail.com>
2025-03-24 11:46:56 -0300

    ex72 resolvido

--- cursos/curso-em-video/mundo-03/ex72.py
+++ cursos/curso-em-video/mundo-03/ex72.py
@@ -2,3 +2,37 @@
 Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso de zero até vinte.
 O programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso
 '''
+
+tupla = (
+    'Zero', 
+    'Um', 
+    'Dois', 
+    'Três', 
+    'Quatro', 
+    'Cinco', 
+    'Seis', 
+    'Sete', 
+    'Oito', 
+    'Nove', 
+    'Dez', 
+    'Onze', 
+    'Doze', 
+    'Treze', 
+    'Quatorze', 
+    'Quinze', 
+    'Dezesseis', 
+    'Dezessete', 
+    'Dezoito', 
+    'Dezenove', 
+    'Vinte'
+)
+
+
+while True:
+    numero = int(input('Digite um número:'))
+    
+    if numero < 0 or numero > 20:
+        continue
+    
+    print(f'O número por extenso é: {tupla[numero]}')
+    break
