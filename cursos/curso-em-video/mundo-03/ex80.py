6a04d026aa07055d56abba1295d6d8b9296e0c16
Roberto <oliveirarobertw@gmail.com>
2025-04-10 11:57:28 -0300

    Create ex80.py

---
+++ cursos/curso-em-video/mundo-03/ex80.py
@@ -0,0 +1,18 @@
+lista = list()
+
+for i in range(0,5):
+    n = int(input('Digite um valor: '))
+    if i == 0 or n > lista[-1]:
+        lista.append(n)
+    else:
+        pos = 0
+        while pos < len(lista):
+             if n <= lista[pos]:
+                lista.insert(pos, n)
+                break
+             pos += 1
+
+print('-='*30)
+print(lista)
+    
+
