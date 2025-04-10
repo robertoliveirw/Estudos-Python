4d596f0f8c8a906f498d34d682a491624d624b74
Roberto <oliveirarobertw@gmail.com>
2025-04-10 15:28:03 -0300

    Create ex82.py

---
+++ cursos/curso-em-video/mundo-03/ex82.py
@@ -0,0 +1,24 @@
+lista   = []
+lista_par =[]
+lista_impar = []
+
+while True:
+    n = int(input('Digite um Número: '))
+
+    lista.append(n)
+
+    c = str(input('Deseja continuar? [S/N]')).upper()
+    if c == 'S':
+        continue
+    else:
+        for i in lista:
+            if i % 2 == 0:
+                lista_par.append(i)
+            else:
+                lista_impar.append(i)
+    
+    print('-='*30)
+    print(f'Lista de números pares: {lista_par}')
+    print('-='*30)
+    print(f'Lista de números ímpares: {lista_impar}')
+    break
